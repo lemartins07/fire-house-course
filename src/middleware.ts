25782a54c58bd61ccfca80907b9ac4e90555e865
Leandro Martins <leandro.martins.85@gmail.com>
2025-07-27 18:29:38 -0300

    Add the admin dashboard page + route protection with Next JS middleware

---
+++ src/middleware.ts
@@ -0,0 +1,28 @@
+import { decodeJwt } from 'jose'
+import { cookies } from 'next/headers'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function middleware(request: NextRequest) {
+  console.log('Middleware: ', request.url)
+
+  if (request.method.toUpperCase() === 'POST') {
+    return NextResponse.next()
+  }
+
+  const cookieStore = await cookies()
+  const token = cookieStore.get('firebaseAuthToken')?.value
+
+  if (!token) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+
+  const decodedToken = decodeJwt(token || '')
+
+  if (!decodedToken.admin) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+}
+
+export const config = {
+  matcher: ['/admin-dashboard'],
+}
