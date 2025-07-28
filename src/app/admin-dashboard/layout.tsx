12ba4d9cd5082f2ef349a9e2a9213218a0e26785
Leandro Martins <leandro.martins.85@gmail.com>
2025-07-27 23:25:40 -0300

    Build the admin dashboard main page

--- src/app/admin-dashboard/layout.tsx
+++ src/app/admin-dashboard/layout.tsx
@@ -1,3 +1,3 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="max-w-screen-sm mx-auto p-5">{children}</div>
+  return <div className="max-w-screen-sm mx-auto px-4 py-10">{children}</div>
 }
